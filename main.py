051f434c83f5bbc2890f795503bed525242f6069
aniruddha <aniruddhamaity911@gmail.com>
2026-07-03 00:57:50 +0530

    fixed error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from services import extract_error_logs
-LOG_FILE_PATH = "D:\DataEngineer\trace-insight\ErrorAnalyzerAgent\resources\enterprise-payment-service.log"
+LOG_FILE_PATH = "D:\\DataEngineer\\trace-insight\\ErrorAnalyzerAgent\\resources\\enterprise-payment-service.log"
 if __name__ == "__main__":
 
     print(extract_error_logs(LOG_FILE_PATH)[0])
