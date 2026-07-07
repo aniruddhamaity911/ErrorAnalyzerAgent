dcf812a630e807507ce60614c054d74efa4dd47f
aniruddha <aniruddhamaity911@gmail.com>
2026-07-08 00:42:08 +0530

    removed .env

---
+++ graph/retriver.py
@@ -0,0 +1,20 @@
+from typing import Dict, Any
+from services import VectorStore
+from state import GraphState
+
+vector_store = VectorStore()
+def retrieve_error_logs(state:GraphState)->Dict[str,Any]:
+    """
+    from the document database i.e. mongodb
+    retrieve relevant error logs
+    :param state: GraphState
+    :return: dictionary
+    """
+    question = state["question"]
+    print("<======== Retrieving error logs ========>")
+    documents = vector_store.search(
+        query=question,
+        k=5
+    )
+    print("<======== Retrieval completed ========>")
+    return {"documents": documents}
